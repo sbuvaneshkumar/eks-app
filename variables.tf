af396bc9984c79e6b89c4eab951b1a3fc078672c
Buvanesh Kumar Sivasubramaniyam <bsivasub@redhat.com>
2022-04-27 12:59:36 +0530

    Terraform formatting

--- variables.tf
+++ variables.tf
@@ -3,53 +3,53 @@ variable "aws_region" {
 }
 
 variable "cluster_version" {
-  type = string
+  type    = string
   default = "1.21"
 }
 
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
-  type = list
+  type    = list(any)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnets" {
-  type = list
-  default = ["10.0.3.0/24","10.0.4.0/24"]
+  type    = list(any)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "node_groups" {
   type = map(object({
-    min_size              = number
-    max_size              = number
-    desired_size          = number
-    instance_type        = string
+    min_size      = number
+    max_size      = number
+    desired_size  = number
+    instance_type = string
   }))
   description = "Managed node group information"
   default = {
     "system-pods" = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size      = 1
+      max_size      = 2
+      desired_size  = 1
       instance_type = "t2.medium"
     }
 
     "cpu-application" = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size      = 1
+      max_size      = 2
+      desired_size  = 1
       instance_type = "t2.medium"
     }
 
     "gpu-application" = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size      = 1
+      max_size      = 2
+      desired_size  = 1
       instance_type = "t2.medium"
+    }
   }
- }
 }
